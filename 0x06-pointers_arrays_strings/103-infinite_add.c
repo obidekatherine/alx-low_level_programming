391e83048a7a29c1ca8ef97fa149dd19e9e708b5
obidekatherine <katherinedenniso@yahoo.com>
2022-10-27 13:45:43 +0100

    new commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,12 +4,6 @@
  * rev_string - reverse array
  * @n: integer params
  * Return: 0
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
  */
 
 void rev_string(char *n)
@@ -33,7 +27,12 @@ void rev_string(char *n)
 }
 
 /**
-
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
